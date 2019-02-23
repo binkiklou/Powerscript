4445fa96e65bb49bb113bdcaebf6b43d94e020e0
Maxence Cossette <maxencek7@gmail.com>
2019-02-22 22:27:45 -0500

    Linux color coding
    Added colors to the interpreter console for linux.

--- src/Log.cpp
+++ src/Log.cpp
@@ -4,6 +4,22 @@
 
 #include <iostream>
 
+/*
+Linux terminal stuff:
+
+Normal \x1B[0m
+Bold \x1B[1m
+Underline \x1B[4m
+
+Red \x1B[31m
+Green \x1B[32m
+Yellow \x1B[33m
+Blue \x1B[34m
+Magenta \x1B[35m
+Cyan \x1B[36m
+White \x1B[37m
+*/
+
 namespace Log
 {
 	void Debug(std::string txt)
@@ -11,33 +27,39 @@ namespace Log
 		if (Powerscript::Debug_Mode == true)
 		{
 			std::cout <<"Debug: "<< txt << std::endl;
-#if OsCode == 1
+#if OsCode == 1 // Windows
 				HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
 				SetConsoleTextAttribute(hcon, 06);
 				std::cout << "Debug: ";
 				SetConsoleTextAttribute(hcon, 07);
 				std::cout << txt << std::endl;
+#elif OsCode == 3 // Linux
+			std::cout << "\x1B[33m" << "Debug:" << "\x1B[0m" << std::endl;
 #endif
 		}
 	}
 	void Update(std::string txt)
 	{
-#if OsCode == 1
+#if OsCode == 1 // Windows
 			HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
 			SetConsoleTextAttribute(hcon, 01);
 			std::cout << "Update: ";
 			SetConsoleTextAttribute(hcon, 07);
 			std::cout << txt << std::endl;
+#elif OsCode == 3 // Linux
+		std::cout << "\x1B[34m" << "Debug:" << "\x1B[0m" << std::endl;
 #endif
 	}
 	void Error(std::string txt)
 	{
-#if OsCode == 1
+#if OsCode == 1 // Windows
 			HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
 			SetConsoleTextAttribute(hcon, 04);
 			std::cout << "Error: ";
 			SetConsoleTextAttribute(hcon, 07);
 			std::cout << txt << std::endl;
+#elif OsCode == 3 // Linux
+		std::cout << "\x1B[31m" << "Debug:" << "\x1B[0m" << std::endl;
 #endif
 	}
 }
