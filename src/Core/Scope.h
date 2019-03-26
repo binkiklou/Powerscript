38c4ff85a54704126cf05da3751a87c67bd78c1f
Maxence <maxencek7@gmail.com>
2019-03-25 20:31:30 -0400

    lmao

--- src/Core/Scope.h
+++ src/Core/Scope.h
@@ -6,5 +6,5 @@ public:
 	int x; // List number at Height
 	int y; //Code Height
 	int p; // Parent's id
-	int id; // Ok so the id would be the position in the vector
+	int id; // Ok so the id would be the height
 };
